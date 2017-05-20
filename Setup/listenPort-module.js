7f7abb5bc82a44dfcb9f8f374485cbf960766c06
asdf6773 <457776773@qq.com>
2017-05-20 13:00:50 +0800

    Setup update with serialport

---
+++ Setup/listenPort-module.js
@@ -0,0 +1,133 @@
+var express = require('express');
+var fs = require('fs');
+var events = require("events");
+var serialport = require("serialport");
+var socket = require('socket.io-client')('http://' + ip + '/serialPort');
+var app = express();
+var server = app.listen(5000);
+
+var flush = false;
+var lastFlush = false;
+var myEmitter = new events.EventEmitter();
+var send = false;
+var preload = JSON.parse(fs.readFileSync('./ip.json', 'utf8'));
+var ip = preload.ip
+
+
+
+
+
+var setup = function(portname) {
+    console.log(ip)
+    openPort(portname);
+
+}
+
+var closePort = function(portname) {
+    var myPort = new serialport(portname, {
+        baudRate: 9600,
+        options: false,
+        parser: serialport.parsers.readline("\r\n")
+    });
+    myPort.close(function(err) {
+        {
+            if (err) {
+                console.log("close port ERROR")
+            } else {
+                console.log("close port")
+            }
+        }
+    })
+}
+
+function openPort(portname) {
+    var myPort = new serialport(portname, {
+        baudRate: 9600,
+        options: false,
+        parser: serialport.parsers.readline("\r\n")
+    });
+    myPort.on('open', function() {
+        console.log('port is open')
+    });
+    myPort.on('close', function() {
+        console.log('port is closed')
+    });
+
+    myPort.on('error', function() {
+        console.log('error')
+    })
+    socket.on("flushIsOver", function() {
+        console.log("flushIsOver")
+        myPort.write("O", function(err) {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('message written');
+        })
+
+    })
+    myPort.on('data', function(data) {
+        if (data.charAt(0) == 'h') { //handDryer
+            // console.log(data)
+            data = data.substr(1);
+            var value = parseInt(data);
+            if (value >= 500) {
+                // flush = true;
+                socket.emit("switchOn", flush)
+                console.log("h" + value)
+            }
+            if (value < 500) {
+                // flush = true;
+                socket.emit("switchOff", flush)
+                console.log("h" + value)
+            }
+
+        }
+        if (data.charAt(0) == 't') { //toilet
+            // console.log(data)
+            data = data.substr(1);
+            var value = parseInt(data);
+            if (value >= 500) {
+                // flush = true;
+                socket.emit("flushPressedFrombutton")
+                console.log("t" + value)
+            }
+
+            if (value < 500) {
+                // flush = true;
+                // socket.emit("switchOff", flush)
+                console.log("t" + value)
+            }
+
+        }
+        if (data.charAt(0) == 'f') { //faucet
+            // console.log(data)
+            data = data.substr(1);
+            var value = parseInt(data);
+            if (value >= 500) {
+                flush = true;
+                socket.emit("faucetOn")
+                console.log("t" + value)
+            }
+            if (value < 500) {
+                flush = true;
+                // socket.emit("switchOff", flush)
+                console.log("t" + value)
+            }
+
+        }
+        setTimeout(function() {
+
+            if (data <= 70) {
+                lastFlush = flush;
+                flush = false;
+                send = false;
+            }
+            // flag=false;
+        }, 200);
+
+    })
+}
+
+module.exports.setup = setup;
+// module.exports.close = closePort;
---
+++ Setup/setup.js
@@ -0,0 +1,98 @@
+var request = require("request")
+var portname = process.argv[2];
+var tunet = require('./tunet');
+var listenPort = require("./listenPort-module")
+var socket = require('socket.io-client')('http://Artisticode.net/setup');
+var childProcess = require('child_process');
+var WiFiControl = require('wifi-control');
+var options = {
+    name: 'y-zhang-13',
+    pwd: '89154055qQ'
+}
+//wifi init
+WiFiControl.init({
+    debug: true
+});
+var ifaceState = WiFiControl.getIfaceState();
+console.log(ifaceState.ssid === undefined)
+
+if (ifaceState.ssid != "Tsinghua" || ifaceState.ssid == undefined) {
+    var _ap = {
+        ssid: "Tsinghua",
+        password: ""
+    };
+    var results = WiFiControl.connectToAP(_ap, function(err, response) {
+        if (err) console.log(err);
+        console.log(response);
+        login();
+    });
+} else {
+    login()
+}
+listenPort.setup("COM16")
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+
+
+// setup();
+//-------------------------------check wifi connection
+function checkInternet(cb) {
+    require('dns').lookup('baidu.com', function(err) {
+        if (err && err.code == "ENOTFOUND") {
+            cb(false);
+        } else {
+            cb(true);
+        }
+    })
+}
+
+//1. check whether it is Tsinghua WIFI
+setInterval(function() {
+    ifaceState = WiFiControl.getIfaceState();
+    if (ifaceState.ssid != "Tsinghua" || ifaceState.ssid == undefined) {
+        var _ap = {
+            ssid: "Tsinghua",
+            password: ""
+        };
+        var results = WiFiControl.connectToAP(_ap, function(err, response) {
+            if (err) console.log(err);
+            login();
+            console.log(response);
+        });
+    } else {
+
+        console.log("status " + socket.connected)
+        if (!socket.connected) {
+            login();
+        }
+        socket.emit("check")
+
+    }
+}, 10000)
+
+//2.check login Status Or Server status
+
+
+
+socket.on("connected", function() {
+    console.log("server connected ")
+})
+//---------------------------------------------------
+
+
+// taskkill / IM chrome.exe
+function login() {
+    setTimeout(function() {
+        tunet.login(options.name, options.pwd, function() {
+            setup();
+        });
+    }, 5000);
+}
+
+function setup() {
+    childProcess.exec("taskkill /IM chrome.exe");
+    setTimeout(function() {
+        childProcess.exec('start chrome --kiosk artisticode.net/mirror --window-position=1920,0 --kiosk --user-data-dir=c:/monitor1');
+        childProcess.exec('start chrome --kiosk artisticode.net/projector --window-position=0,0 --kiosk --user-data-dir=c:/monitor2')
+    }, 5000)
+}
